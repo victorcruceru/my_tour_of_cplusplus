f176b9c70b21e0c66fcedf24b6767151d7db17ec
Victor Cruceru <victor.cruceru@oracle.com>
2019-01-07 09:32:02 -0800

    Tuple unpack example. Added some copyright texts

--- src/lambda1.cpp
+++ src/lambda1.cpp
@@ -1,3 +1,18 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * Victor Cruceru wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return.
+ * ----------------------------------------------------------------------------
+ */
+
+/*
+ * Description: Basic attempts to use lambda functions.
+ * The most interesting item is the recursive ( impractical! ) Fibonacci 
+ * function.
+ */
+ 
 #include <cstdlib>
 #include <iostream>
 #include <functional>
@@ -7,33 +22,33 @@ int main(){
   auto is_positive = [] (int n) { 
     return (n >= 0);
   };
-  std::cout<< std::boolalpha << is_positive(7) <<std::endl;
+  std::cout << std::boolalpha << is_positive(7) << std::endl;
 
   // capture all local names by values 
   auto zero = 0;
   auto is_positive2 = [=] (int n) { 
     return (n >= zero);
   };
-  std::cout<< std::boolalpha << is_positive2(-2) <<std::endl;
+  std::cout << std::boolalpha << is_positive2(-2) << std::endl;
   
   // capture only zero name by value
   auto is_positive3 = [zero] (int n) { 
     return (n >= zero);
   };
-  std::cout<< std::boolalpha << is_positive3(-2) <<std::endl;
+  std::cout << std::boolalpha << is_positive3(-2) << std::endl;
 
   // capture only zero name by reference
   auto is_positive4 = [&zero] (int n) { 
     return (n >= zero);
   };
-  std::cout<< std::boolalpha << is_positive4(-0) <<std::endl;
+  std::cout << std::boolalpha << is_positive4(-0) << std::endl;
 
   // recursive Fibonacci lambda function
   std::function <uint64_t(uint64_t)> fib = [&fib] (uint64_t n) { 
-    return n < 2 ? 1 : fib(n - 1) + fib(n - 2);
+    return (n == 0 ? 0 : ( n == 1 ? 1 : fib(n - 1) + fib(n - 2)));
   };
 
-  std::cout<< fib(10) <<std::endl;
+  std::cout<< fib(17) <<std::endl;
 
   return(EXIT_SUCCESS);
 }
---
+++ src/tuple3.cpp
@@ -0,0 +1,41 @@
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * Victor Cruceru wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return.
+ * ----------------------------------------------------------------------------
+ */
+
+/*
+ * Description: How to unpack a tuple
+ */ 
+#include <cstdlib>
+#include <iostream>   // std::cout
+#include <string> 
+#include <tuple>
+#include <ctime>
+#include <chrono>
+#include <cstdlib>
+
+static
+std::tuple<std::string, std::chrono::system_clock::time_point, int32_t> 
+get_stock(const std::string& s_symbol){
+  return std::make_tuple(s_symbol,  
+                  std::chrono::system_clock::now() + std::chrono::hours(24),
+                  std::rand()); 
+  
+}
+
+int main(int argc, char** argv){
+  std::srand(std::time(nullptr)); 
+  for (int n : {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}){
+    const auto [name, valid_time, price] = get_stock("AMZN" + 
+                                                     std::to_string(n));
+    std::cout<<name<<", "<< std::chrono::system_clock::to_time_t(valid_time) 
+             << ", " << price << std::endl;
+  }
+  return(EXIT_SUCCESS);
+}  
+
+ 
