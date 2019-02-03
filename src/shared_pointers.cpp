bebcc8f448eab152bc95e68e9d7a28acd9711b84
VCRUCERU <victor.cruceru@gmail.com>
2019-02-04 00:08:16 +0200

    Use UTF-8 / string prefix u8

--- src/shared_pointers.cpp
+++ src/shared_pointers.cpp
@@ -18,35 +18,32 @@
 #include <memory> // std::make_shared
 #include <locale>
 
-#include <io.h>
-#include <fcntl.h>
-
 class Poem{
   public:
-    using lyrics_t = std::vector<std::wstring>;
+    using lyrics_t = std::vector<std::string>;
 
   private:
-    std::wstring title_ = L"";
-    std::wstring author_ = L"";
+    std::string title_ = u8"";
+    std::string author_ = u8"";
     lyrics_t lyrics_ = {};
 
   public:
     Poem(){
-      std::wcout << "Calling *Default* Constructor" <<  std::hex 
+      std::cout << "Calling *Default* Constructor" <<  std::hex 
                  << ", this = " << this << std::endl;
     }
 
-    Poem(const std::wstring& title, const std::wstring& author, 
+    Poem(const std::string& title, const std::string& author, 
          const lyrics_t& lyrics): 
         title_{title}, author_{author}, lyrics_{lyrics}{
-      std::wcout << "Calling Constructor for: " << title <<", " << author
+      std::cout << "Calling Constructor for: " << title <<", " << author
                  <<  std::hex << ", this = " << this << std::endl;
 
     } 
      
     Poem(const Poem& other): title_{other.title_}, author_{other.author_}, \
                               lyrics_{other.lyrics_} {
-      std::wcout << "Calling *Copy* Constructor" <<  std::hex 
+      std::cout << "Calling *Copy* Constructor" <<  std::hex 
                  << ", this = " << this << std::endl;
 
     } 
@@ -55,14 +52,14 @@ class Poem{
       title_{std::move(other.title_)}, 
       author_{std::move(other.author_)}, 
       lyrics_{std::move(other.lyrics_)}  {
-      std::wcout << "Calling *Move* Constructor" <<  std::hex 
+      std::cout << "Calling *Move* Constructor" <<  std::hex 
                  << ", this = " << this << std::endl;
      
     } 
 
 
     Poem& operator=(const Poem& other){
-      std::wcout << "Calling assignment operator" << std::hex 
+      std::cout << "Calling assignment operator" << std::hex 
                  << ", this = " << this << std::endl;
       title_ = other.title_;
       author_ = other.author_;
@@ -71,7 +68,7 @@ class Poem{
     } 
 
     Poem& operator=(Poem&& other){
-      std::wcout << "Calling *move* assignment operator" <<  std::hex 
+      std::cout << "Calling *move* assignment operator" <<  std::hex 
                  << ", this = " << this << std::endl;
       title_ = std::move(other.title_);
       author_ = std::move(other.author_);
@@ -80,15 +77,15 @@ class Poem{
     } 
 
     ~Poem() noexcept {
-      std::wcout << "Calling Destructor for: " << title_ << ", " << author_ 
+      std::cout << "Calling Destructor for: " << title_ << ", " << author_ 
                  << std::hex << ", this = " << this << std::endl;
     } 
     
-    const std::wstring& get_title(void) const{
+    const std::string& get_title(void) const{
       return this->title_;
     }
 
-    const std::wstring& get_author(void) const{
+    const std::string& get_author(void) const{
       return this->author_;
     }  
 
@@ -96,26 +93,24 @@ class Poem{
       return this->lyrics_;
     }
 
-    void set_author(const std::wstring& author){
+    void set_author(const std::string& author){
       this->author_= author;
     }
 
 };
 
 int main(){
-  //std::locale::global(std::locale("en_US.utf8"));
-  //std::wcout.imbue(std::locale());
-
-  
-  
-  //_setmode(_fileno(stdout), _O_U16TEXT);
+#if not defined(_WIN32)
+  std::locale::global(std::locale("en_US.utf8"));
+  std::cout.imbue(std::locale());
+#endif
 
   Poem p1 {
-    L"Venus and Adonis",
-    L"William Shakespeare",    
+    u8"Venus and Adonis",
+    u8"William Shakespeare",    
     {
-      L"EVEN as the sun with purple-colour'd face", 
-      L"Had ta'en his last leave of the weeping morn,"
+      u8"EVEN as the sun with purple-colour'd face", 
+      u8"Had ta'en his last leave of the weeping morn,"
     } 
   };
    
@@ -126,45 +121,44 @@ int main(){
   auto sp1 = std::make_shared<Poem>(p1);  // Copy Constructor will be called
 
   
-  std::wcout << "--->Empty Shared pointer ..." << std::endl;
+  std::cout << "--->Empty Shared pointer ..." << std::endl;
   auto sp2 = std::make_shared<Poem>();    // Default Constructor will be called
   
-  std::wcout << "--->Shared pointer for Hugo #1...." << std::endl;
+  std::cout << "--->Shared pointer for Hugo #1...." << std::endl;
   auto sp3 = std::make_shared<Poem>(
-      L"LA CONSCIENCE",
-      L"Victor Hugo",       
+      u8"LA CONSCIENCE",
+      u8"Victor Hugo",       
       Poem::lyrics_t {
-        L"Lorsque avec ses enfants vêtus de peaux de bêtes,", 
-        L"Échevelé, livide au milieu des tempêtes,",
-        L"Caïn se fut enfui de devant Jéhovah,",
-        L"....",
-        //L"Comme le soir tombait, l’homme sombre arriva",
-        //L"Au bas d’une montagne en une grande plaine ;"
+        u8"Lorsque avec ses enfants vêtus de peaux de bêtes,", 
+        u8"Échevelé, livide au milieu des tempêtes,",
+        u8"Caïn se fut enfui de devant Jéhovah,",
+        u8"Comme le soir tombait, l’homme sombre arriva",
+        u8"Au bas d’une montagne en une grande plaine ;"
       }      
   );
 
-  sp3->set_author(L"'" + sp3->get_author() + L"'");
-  std::wcout << "DEBUG/ sp3 Poem: "<< sp3->get_author() << ": " 
+  sp3->set_author(u8"'" + sp3->get_author() + u8"'");
+  std::cout << "DEBUG/ sp3 Poem: "<< sp3->get_author() << ": " 
              << sp3->get_title() << " verses count = " 
              << sp3->get_lyrics().size() << std::endl;
 
   for (auto& verse: sp3->get_lyrics()){
-    std::wcout << "\t" << verse << std::endl;
+    std::cout << "\t" << verse << std::endl;
   }           
 
-  std::wcout << "--->Shared pointer for Hugo #2...." << std::endl;
+  std::cout << "--->Shared pointer for Hugo #2...." << std::endl;
    
   std::shared_ptr<Poem> sp4 {
     new Poem(
-      L"PUISSANCE ÉGALE BONTÉ",      
-      L"Victor Hugo", 
+      u8"PUISSANCE ÉGALE BONTÉ",      
+      u8"Victor Hugo", 
       
       Poem::lyrics_t {
-        L"Au commencement, Dieu vit un jour dans l’espace", 
-        L"Iblis venir à lui ; Dieu dit : « Veux-tu ta grâce ?",
-        L"— Non, dit le Mal. — Alors que me demandes-tu ?",
-        L"— Dieu, répondit Iblis de ténèbres vêtu,",
-        L"Joutons à qui créera la chose la plus belle."
+        u8"Au commencement, Dieu vit un jour dans l’espace", 
+        u8"Iblis venir à lui ; Dieu dit : « Veux-tu ta grâce ?",
+        u8"— Non, dit le Mal. — Alors que me demandes-tu ?",
+        u8"— Dieu, répondit Iblis de ténèbres vêtu,",
+        u8"Joutons à qui créera la chose la plus belle."
       }  
     )
   };
@@ -177,13 +171,13 @@ int main(){
   sp4_copy2.reset();
 
   
-  std::wcout << "--->Shared pointer for an int...." << std::endl;
+  std::cout << "--->Shared pointer for an int...." << std::endl;
   std::shared_ptr<int> ptr(new int);
   assert(ptr.use_count() == 1);
   std::shared_ptr<int> ptr_copy = ptr;
   assert(ptr.use_count() == 2);
   *ptr_copy = 10777;
-  std::wcout << *ptr << std::endl;
+  std::cout << *ptr << std::endl;
   ptr.reset();
   assert(ptr.use_count() == 0);
 
